dfded6d55458367a032c2519ecdc2103453dbda3
huangyuetian <q529658851@gmail.com>
2016-08-08 19:55:09 +0800

    add admin css and remove not use themes

--- AdminAsset.php
+++ AdminAsset.php
@@ -8,16 +8,14 @@ use yii\web\AssetBundle;
  * @author Rany Huang <q529658851@gmail.com>
  * @since 2.0
  */
-class ExtjsAsset extends AssetBundle
+class AdminAsset extends AssetBundle
 {
     public $sourcePath = __DIR__;
     // public $baseUrl = '@web';
     public $css = [
-        'classic/theme-triton/resources/theme-triton-all.css',
+        'resources/Admin-all.css',
     ];
     public $js = [
-    	'ext-all.js',
-    	'classic/theme-classic/theme-classic.js'
     ];
     public $depends = [
     ];
