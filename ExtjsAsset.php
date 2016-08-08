12f762d5cd551e28bdd29f8bf89e726e796cbabe
huangyuetian <q529658851@gmail.com>
2016-08-08 19:04:54 +0800

    add composer info

---
+++ ExtjsAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace gcms\extjs\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class ExtjsAsset extends AssetBundle
+{
+    public $sourcePath = '@vender/gcms/extjs';
+    // public $baseUrl = '@web';
+    public $css = [
+        'classic/theme-triton/resources/theme-triton-all.css',
+    ];
+    public $js = [
+    	'ext-all.js',
+    	'classic/theme-classic/theme-classic.js'
+    ];
+    public $depends = [
+    ];
+}
